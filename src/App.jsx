f52d20d4ffd2f7c016d520d2f4d43aa1f43e8ff1
unknown <arizfadillaht02@gmail.com>
2026-06-07 21:38:35 +0700

    deploy website vercell

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
                   <Hero />
                   <TechnicalProficiency />
                   <AboutMe />
-                  <MyPorfolio />
+                  <MyProjects />
                   <ContactMe />
                   <Footer />
             </div>
